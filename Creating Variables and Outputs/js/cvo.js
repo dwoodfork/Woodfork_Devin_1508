74803abfe020c7be09171070ada6041b8964e3b4
dwoodfork <dmwoodfork@fullsail.edu>
2015-08-06 23:57:32 -0500

    created alert box
    displaying information

--- Creating Variables and Outputs/js/cvo.js
+++ Creating Variables and Outputs/js/cvo.js
@@ -4,4 +4,27 @@
  * Creating Variables and Output.
  */
 
-console.log("Hello world!");
+
+ var age;
+ age = 23;
+
+ var fullName;
+ name = "Devin Woodfork";
+
+ var city;
+ city = "Memphis";
+
+ var isLegal = true;
+
+ var isIllegal = false;
+
+  isLegal >= 18
+
+  isIllegal < 18
+
+
+
+
+
+alert ("My name is" + " " + name + " " + "and I am" + " " + age + "." + " " +
+    "I am from" + " " + city + " and according to my age when asked if I am of legal" + " age the answer is always" + " " +  isLegal + "." + "I am a Web Design and Developer Major." );
